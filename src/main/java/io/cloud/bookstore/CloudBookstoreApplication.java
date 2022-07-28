b800efe5ce66432ad08b7dd2ee704a2749d553a7
liguirong <liguirong@toptolink.com>
2022-07-28 21:57:05 +0800

    初始化仓库

---
+++ src/main/java/io/cloud/bookstore/CloudBookstoreApplication.java
@@ -0,0 +1,13 @@
+package io.cloud.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudBookstoreApplication.class, args);
+    }
+
+}
